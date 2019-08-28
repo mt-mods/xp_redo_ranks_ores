af83f50dab6349d5cc3bc47cef25689289607455
NatureFreshMilk <naturefreshmilk@github.com>
2019-08-28 15:17:38 +0200

    luacheck + new rank

--- ranks.lua
+++ ranks.lua
@@ -1,16 +1,71 @@
 
 local prefixes = {
-	{name="Cobble", icon="xp_cobble.png", color={r=255, g=255, b=255}, base_xp=0, xp_modifier=100}, -- 0 - 1k
-	{name="Stone", icon="xp_stone_block.png", color={r=164, g=164, b=164}, base_xp=2000, xp_modifier=200}, -- 2k - 4k
-	{name="Coal", icon="xp_coal_block.png", color={r=99, g=99, b=99}, base_xp=5000, xp_modifier=500}, -- 5k - 10k
-	{name="Iron", icon="xp_steel_block.png", color={r=214, g=215, b=216}, base_xp=20000, xp_modifier=6000}, -- 20k - 80k
-	{name="Copper", icon="xp_copper_block.png", color={r=209, g=188, b=115}, base_xp=100000, xp_modifier=10000}, -- 100k - 200k
-	{name="Gold", icon="xp_gold_block.png", color={r=196, g=162, b=39}, base_xp=300000, xp_modifier=50000}, -- 300k - 800k
-	{name="Diamond", icon="xp_diamond_block.png", color={r=85, g=210, b=252}, base_xp=1000000, xp_modifier=80000}, -- 1000k - 1800k
-	{name="Mese", icon="xp_mese_block.png", color={r=238, g=252, b=85}, base_xp=2000000, xp_modifier=300000} -- 2000k - 5000k
+	{
+		name="Cobble",
+		icon="xp_cobble.png",
+		color={r=255, g=255, b=255},
+		base_xp=0,
+		xp_modifier=100
+	}, -- 0 - 1k
+	{
+		name="Stone",
+		icon="xp_stone_block.png",
+		color={r=164, g=164, b=164},
+		base_xp=2000,
+		xp_modifier=200
+	}, -- 2k - 4k
+	{
+		name="Coal",
+		icon="xp_coal_block.png",
+		color={r=99, g=99, b=99},
+		base_xp=5000,
+		xp_modifier=500
+	}, -- 5k - 10k
+	{
+		name="Iron",
+		icon="xp_steel_block.png",
+		color={r=214, g=215, b=216},
+		base_xp=20000,
+		xp_modifier=6000
+	}, -- 20k - 80k
+	{
+		name="Copper",
+		icon="xp_copper_block.png",
+		color={r=209, g=188, b=115},
+		base_xp=100000,
+		xp_modifier=10000
+	}, -- 100k - 200k
+	{
+		name="Gold",
+		icon="xp_gold_block.png",
+		color={r=196, g=162, b=39},
+		base_xp=300000,
+		xp_modifier=50000
+	}, -- 300k - 800k
+	{
+		name="Diamond",
+		icon="xp_diamond_block.png",
+		color={r=85, g=210, b=252},
+		base_xp=1000000,
+		xp_modifier=80000
+	}, -- 1000k - 1800k
+	{
+		name="Mese",
+		icon="xp_mese_block.png",
+		color={r=238, g=252, b=85},
+		base_xp=2000000,
+		xp_modifier=300000
+	}, -- 2000k - 5000k
+	{
+		name="Lava",
+		icon="xp_lava.png",
+		color={r=238, g=30, b=30},
+		base_xp=5000000,
+		xp_modifier=500000
+	} -- 5000k - 10M
 }
 
--- further ideas: lava, ?
+-- further ideas: mithril?
 
 local suffixes = {
 	-- 7 suffixes
