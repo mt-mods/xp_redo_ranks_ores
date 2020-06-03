28b779112ebb6c5113be25e7ab6e09141e052c7d
OgelGames <olliverdc28@gmail.com>
2020-06-03 21:25:40 +1000

    adjust xp values and add uranium rank
    * adjusted xp values so none of the new ranks overlap
    * evened out xp values so they gradually increase
    * added missing uranium rank

--- ranks.lua
+++ ranks.lua
@@ -25,72 +25,79 @@ local prefixes = {
 		name="Iron",
 		icon="xp_steel_block.png",
 		color={r=214, g=215, b=216},
-		base_xp=20000,
+		base_xp=20*1000,
 		xp_modifier=6000
 	}, -- 20k - 80k
 	{
 		name="Copper",
 		icon="xp_copper_block.png",
 		color={r=209, g=188, b=115},
-		base_xp=100000,
-		xp_modifier=10000
+		base_xp=100*1000,
+		xp_modifier=10*1000
 	}, -- 100k - 200k
 	{
 		name="Gold",
 		icon="xp_gold_block.png",
 		color={r=196, g=162, b=39},
-		base_xp=300000,
-		xp_modifier=50000
+		base_xp=300*1000,
+		xp_modifier=50*1000
 	}, -- 300k - 800k
 	{
 		name="Diamond",
 		icon="xp_diamond_block.png",
 		color={r=85, g=210, b=252},
-		base_xp=1000000,
-		xp_modifier=80000
-	}, -- 1000k - 1800k
+		base_xp=1*1000*1000,
+		xp_modifier=80*1000
+	}, -- 1M - 1.8M
 	{
 		name="Mese",
 		icon="xp_mese_block.png",
 		color={r=238, g=252, b=85},
-		base_xp=2000000,
-		xp_modifier=300000
-	}, -- 2000k - 5000k
+		base_xp=2*1000*1000,
+		xp_modifier=200*1000
+	}, -- 2M - 4M
 	{
 		name="Lava",
 		icon="xp_lava.png",
 		color={r=238, g=30, b=30},
-		base_xp=6*1000*1000,
-		xp_modifier=400*1000
-	}, -- 6000k - 10M
+		base_xp=5*1000*1000,
+		xp_modifier=300*1000
+	}, -- 5M - 8M
 	{
 		name="Sulfur",
 		icon="xp_sulfur.png",
 		color={r=255, g=237, b=77},
-		base_xp=10*1000*1000,
-		xp_modifier=200*1000
-	}, -- 10M - 12M
+		base_xp=9*1000*1000,
+		xp_modifier=300*1000
+	}, -- 9M - 12M
+		{
+		name="Uranium",
+		icon="xp_uranium.png",
+		color={r=135, g=209, b=125},
+		base_xp=13*1000*1000,
+		xp_modifier=400*1000
+	}, -- 13M - 17M
 	{
 		name="Chernobylite",
 		icon="xp_chernobylite.png",
 		color={r=30, g=138, b=30},
-		base_xp=12*1000*1000,
-		xp_modifier=300*1000
-	}, -- 12M - 15M
+		base_xp=18*1000*1000,
+		xp_modifier=400*1000
+	}, -- 18M - 22M
 	{
 		name="Corium",
 		icon="xp_corium.png",
 		color={r=30, g=238, b=30},
-		base_xp=15*1000*1000,
-		xp_modifier=300*1000
-	}, -- 15M - 18M
+		base_xp=23*1000*1000,
+		xp_modifier=500*1000
+	}, -- 23M - 28M
 	{
 		name="Mithril",
 		icon="xp_mithril.png",
 		color={r=49, g=79, b=232},
-		base_xp=18*1000*1000,
-		xp_modifier=300*1000
-	} -- 18M - 21M
+		base_xp=29*1000*1000,
+		xp_modifier=600*1000
+	}, -- 29M - 35M
 }
 
 local suffixes = {
